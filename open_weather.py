5609faa744fd1066ccdc1b293c6b1fb0c8e8725f
emoon <sertvitas@gmail.com>
2022-08-22 15:51:34 -0400

    open_weather api calls/json

--- open_weather.py
+++ open_weather.py
@@ -1,13 +1,17 @@
 import requests
 
 api_key = "9ca3700ce771a59d1cd0c6ec305030d5"
+
 city = "Townsend"
 state = "MA"
+country = "US"
 
 lat="42.7129142"
 long="-71.7385216"
 
-url = "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+long+"&appid="+api_key
+##url = "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+long+"&appid="+api_key+"&units=imperial"
+
+url = "http://api.openweathermap.org/data/2.5/weather?q="+city+","+state+","+country+"&appid="+api_key+"&units=imperial"
 
 print(url)
 
